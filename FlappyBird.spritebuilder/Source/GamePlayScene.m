a03fa48f253bf8cf98d98ff70dabe6429c14c62f
danicarr <danicarr@mit.edu>
2015-01-07 01:23:53 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -12,9 +12,11 @@
 
 -(void)update:(CCTime)delta
 {
-    // put update code here
+    [character flap];
 }
 
-// put new methods here
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    
+}
 
 @end
